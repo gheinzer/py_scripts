f80837478a30065ad1e2884f47a2760549af6c3b
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-12-01 20:42:07 +0100

    Added linux-add-command-alias.py

---
+++ linux-add-command-alias.py
@@ -0,0 +1,42 @@
+"""
+Author: Gabriel Heinzer (dev@gabrielheinzer.ch)
+linux-add-command-alias.py (c) 2021
+Desc: This script helps adding command aliases in linux.
+Created:  2021-12-01T19:33:33.153Z
+Modified: 2021-12-01T19:41:54.881Z
+"""
+import os
+import sys
+import platform
+def main():
+    if("-h" in sys.argv):
+        print("""
+linux-add-service by Gabriel Heinzer
+These are the arguments you can pass:
+    -f  Force the execution and ignore detected OS.
+    -h  Display this help
+        """)
+        exit()
+    if(platform.system() != "Linux" and not "-f" in sys.argv):
+        print("You do not seem to run Linux. If you execute this on linux, but this error message appears, try running this script with -f.")
+        exit()
+    if(not os.path.exists("~/.bashrc")):
+        print("~/.bashrc file does not exist.")
+        exit()
+
+    inputValid = False;
+    while inputValid:
+        name = input("What should the name of the alias, the short command, be?: ")
+        cmd = input("For which command should it be an alias (which command should be executed when the alias name is executed)?: ")
+
+        if("" in (name, cmd)):
+            inputValid = False
+            print("Your input is invalid. Please try again.")
+            continue
+        else:
+            inputValid = True
+
+    print("Writing file...")
+    bashrc = open("~/.bashrc", "a")
+    bashrc.write(f'\nalias {name}="{cmd}"')
+    print("Finished.")
