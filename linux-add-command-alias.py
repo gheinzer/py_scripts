fb664bebb9dcd00b494633b10bdaa97723481abd
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-12-01 20:44:23 +0100

    No output was printed out again. Made one more fix

--- linux-add-command-alias.py
+++ linux-add-command-alias.py
@@ -3,7 +3,7 @@ Author: Gabriel Heinzer (dev@gabrielheinzer.ch)
 linux-add-command-alias.py (c) 2021
 Desc: This script helps adding command aliases in linux.
 Created:  2021-12-01T19:33:33.153Z
-Modified: 2021-12-01T19:43:05.660Z
+Modified: 2021-12-01T19:43:53.539Z
 """
 import os
 import sys
@@ -24,7 +24,7 @@ These are the arguments you can pass:
         print("~/.bashrc file does not exist.")
         exit()
 
-    inputValid = False;
+    inputValid = False
     while not inputValid:
         name = input("What should the name of the alias, the short command, be?: ")
         cmd = input("For which command should it be an alias (which command should be executed when the alias name is executed)?: ")
