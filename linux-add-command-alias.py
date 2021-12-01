4488e3f1847f17acc2ede7ddc6347b7091c33b8a
Gabriel Heinzer <dev@gabrielheinzer.ch>
2021-12-01 20:45:37 +0100

    Main function was not even executed. Fixed this and added a title.

--- linux-add-command-alias.py
+++ linux-add-command-alias.py
@@ -3,15 +3,16 @@ Author: Gabriel Heinzer (dev@gabrielheinzer.ch)
 linux-add-command-alias.py (c) 2021
 Desc: This script helps adding command aliases in linux.
 Created:  2021-12-01T19:33:33.153Z
-Modified: 2021-12-01T19:43:53.539Z
+Modified: 2021-12-01T19:45:32.055Z
 """
 import os
 import sys
 import platform
 def main():
+    print("linux-add-command-alias by Gabriel Heinzer")
     if("-h" in sys.argv):
         print("""
-linux-add-service by Gabriel Heinzer
+linux-add-command-alias by Gabriel Heinzer
 These are the arguments you can pass:
     -f  Force the execution and ignore detected OS.
     -h  Display this help
@@ -40,3 +41,6 @@ These are the arguments you can pass:
     bashrc = open("~/.bashrc", "a")
     bashrc.write(f'\nalias {name}="{cmd}"')
     print("Finished.")
+
+if __name__ == "__main__":
+    main()
